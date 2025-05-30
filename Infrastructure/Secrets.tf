9f55b9db941dd00f28785a283d0810f6c121530d
Vaibhavkumar Patel <vaibhavpatel162002@gmail.com>
2025-05-30 16:51:03 -0300

    refactor: Un-comment and update Secrets.tf to define LLM and Qdrant endpoints

--- Infrastructure/Secrets.tf
+++ Infrastructure/Secrets.tf
@@ -1,13 +1,12 @@
-# data "aws_secretsmanager_secret" "llm_ip" {
-#   name = "prod/QGpt"
-# }
+data "aws_secretsmanager_secret" "llm_ip" {
+  name = "prod/QGpt"
+}
 
-# resource "aws_secretsmanager_secret_version" "llm_ip" {
-#   secret_id     = data.aws_secretsmanager_secret.llm_ip.id
-#   secret_string = jsonencode({
-#     llm_endpoint     = aws_lb.llm.dns_name
-#     llm_port         = 11434
-#     vectordb_endpoint = aws_lb.qdrant.dns_name
-#     vectordb_port    = 80
-#   })
-# }
+resource "aws_secretsmanager_secret_version" "llm_ip" {
+  secret_id     = data.aws_secretsmanager_secret.llm_ip.id
+  secret_string = jsonencode({
+     OLLAMA_HOST = "http://${aws_instance.llm_instance.private_ip}:11434"
+     QDRANT_HOST = "http://${aws_lb.qdrant_lb.dns_name}"
+     QDRANT_PORT = 6333
+  })
+}
